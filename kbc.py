b2c8b75fe70cae076587c47363dfc3137468dd2e
Ripon R. Rahman <74227389+RiponUzumaki@users.noreply.github.com>
2024-11-06 07:06:48 +0600

    Initial Commit

---
+++ kbc.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+print(name)
+print("Answer the right question.")
+print("Which fruit is red color?")
+l = ["Apple", "Orrange", "Banana", "Mango"]
+print(l)
+print("Answer your question carefully.")
+answer = input("Answer: ")
+
+if answer == ("Apple"):
+    print("You won 1 crore!")
+else:
+    print("Your answer is wrong, Sorry!")  
